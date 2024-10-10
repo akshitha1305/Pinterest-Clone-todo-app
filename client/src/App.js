8532d45f5816a1c28cc11f166f3972932d5f50c9
soumya508 <soumyag316@gmail.com>
2024-10-10 01:08:34 -0500

    navbar, formlayout, pingrids and pins will be displayed

--- client/src/App.js
+++ client/src/App.js
@@ -4,9 +4,10 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import FormLayout from "./components/FormLayout";
 import Login from "./pages/Login";
-
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
 import Signup from "./pages/Signup";
-
+import Search from "./pages/Search";
 import { RequireAuth, NotRequireAuth } from "./routes/routes";
 
 const App = () => {
@@ -31,6 +32,30 @@ const App = () => {
             }
           />
         </Route>
+        <Route
+          path="/profile"
+          element={
+            <RequireAuth>
+              <Profile />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/search/:query"
+          element={
+            <RequireAuth>
+              <Search />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/"
+          element={
+            <RequireAuth>
+              <Home />
+            </RequireAuth>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
