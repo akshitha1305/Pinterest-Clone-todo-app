8ef9e98af68e983feefd8f8165ed0070a1de441e
akshitha1305 <akshithamerugu321@gmail.com>
2024-10-24 18:57:21 -0500

    implement follow/unfollow functionality with user follower retrieval

--- client/src/services/users.js
+++ client/src/services/users.js
@@ -24,3 +24,32 @@ export const deleteSavedPin = async ({ userId, photoUrl }) => {
   return await instance.put(`/${userId}/delete-pin`, { photoUrl });
 };
 
+// Follow / Unfollow
+export const checkIfFollowing = async (pin_owner_id) => {
+  const loggedInUserId = localStorage.getItem('userId');
+  const response = await instance.post(`/check-follow/user`, { loggedInUserId, pin_owner_id });
+  return response.data.isFollowing; // Assuming the response contains a boolean `isFollowing`
+};
+
+// Follow a user
+export const followUser = async (userIdToFollow) => {
+  const loggedInUserId = localStorage.getItem('userId');
+  return await instance.post(`/follow/user`, { userIdToFollow, loggedInUserId });
+};
+
+// Unfollow a user
+export const unfollowUser = async (userIdToUnfollow) => {
+  const loggedInUserId = localStorage.getItem('userId');
+  return await instance.post(`/unfollow/user`, { userIdToUnfollow, loggedInUserId });
+};
+
+// Get followers of a user
+export const getFollowers = async (userId) => {
+  return await instance.get(`/${userId}/followers`);
+};
+
+// Get users the logged-in user is following
+export const getFollowing = async (userId) => {
+  return await instance.get(`/${userId}/following`);
+};
+
