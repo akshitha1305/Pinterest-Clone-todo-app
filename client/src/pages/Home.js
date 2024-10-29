754a6bb65df378cc673fc7585dd3e60a1bfb564e
pso011 <poojithasoma2805@gmail.com>
2024-10-28 22:13:56 -0500

    Addition of liked State in Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,16 +2,59 @@ import React, { useEffect } from "react";
 
 import { useDispatch, useSelector } from "react-redux";
 
-import { getSavedPins,getLikedPins, getRandomPins } from "../actions/pin";
+// import { getSavedPins, getRandomPins } from "../actions/pin";
 import NavBar from "../components/NavBar";
 import PinGrid from "../components/PinGrid";
+import { getSavedPins, getLikedPins, getRandomPins } from "../actions/pin";
+
+// const Home = () => {
+//   const dispatch = useDispatch();
+//   const user = useSelector((state) => state.session.user);
+//   const userId = user.id;
+
+//   const { feed, saved } = useSelector((state) => state.pin);
+
+//   useEffect(() => {
+//     dispatch(getSavedPins({ userId, setAsFeed: false }));
+//     dispatch(getRandomPins());
+//   }, [dispatch, userId]);
+
+//   return (
+//     <div>
+//       <NavBar />
+//       <PinGrid userId={userId} pins={feed} savedPins={saved} />
+//     </div>
+//   );
+// };
+
+
+// const Home = () => {
+//   const dispatch = useDispatch();
+//   const user = useSelector((state) => state.session.user);
+//   const userId = user.id;
+
+//   const { feed, saved, liked } = useSelector((state) => state.pin); // Add liked state
+
+//   useEffect(() => {
+//     dispatch(getSavedPins({ userId, setAsFeed: false }));
+//     dispatch(getLikedPins({ userId, setAsFeed: false })); // Fetch liked pins
+//     dispatch(getRandomPins());
+//   }, [dispatch, userId]);
+
+//   return (
+//     <div>
+//       <NavBar />
+//       <PinGrid userId={userId} pins={feed} savedPins={saved} likedPins={liked} /> {/* Pass likedPins to PinGrid */}
+//     </div>
+//   );
+// };
 
 const Home = () => {
   const dispatch = useDispatch();
   const user = useSelector((state) => state.session.user);
   const userId = user.id;
 
-  const { feed, saved, liked} = useSelector((state) => state.pin);
+  const { feed, saved, liked } = useSelector((state) => state.pin);
 
   useEffect(() => {
     dispatch(getSavedPins({ userId, setAsFeed: false }));
@@ -22,9 +65,10 @@ const Home = () => {
   return (
     <div>
       <NavBar />
-      <PinGrid userId={userId} pins={feed} savedPins={saved}  likedPins={liked} />
+      <PinGrid userId={userId} pins={feed} savedPins={saved} likedPins={liked} />
     </div>
   );
 };
 
 export default Home;
+
