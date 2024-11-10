c007864f4182012cb5a3bd84d550bbe98e1a6706
pso011 <poojithasoma2805@gmail.com>
2024-11-09 18:10:09 -0600

    Update NavBar.js

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,3 +1,192 @@
+// // import React from "react";
+
+// // import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+// // import LogoutIcon from "@mui/icons-material/Logout";
+// // import PinterestIcon from "@mui/icons-material/Pinterest";
+// // import SearchIcon from "@mui/icons-material/Search";
+// // import { useDispatch } from "react-redux";
+// // import { Link, NavLink, useNavigate } from "react-router-dom";
+
+// // import "./NavBar.css";
+// // import { logout } from "../actions/session";
+
+// // const NavBar = ({ query }) => {
+// //   const dispatch = useDispatch();
+// //   const navigate = useNavigate();
+
+// //   const handleSearch = (event) => {
+// //     event.preventDefault();
+// //     navigate(`/search/${event.target.query.value}`);
+// //   };
+
+// //   const handleLogout = () => dispatch(logout());
+
+// //   return (
+// //     <div className="nav-bar">
+// //       <div className="nav-bar__icon nav-bar__icon--red">
+// //         <Link to="/">
+// //           <PinterestIcon />
+// //         </Link>
+// //       </div>
+// //       <div className="nav-bar__link">
+// //         <NavLink
+// //           to="/"
+// //           className={({ isActive }) =>
+// //             isActive ? "nav-bar__link--active" : "nav-bar__link--inactive"
+// //           }
+// //         >
+// //           Home
+// //         </NavLink>
+
+// //          <NavLink
+// //           to="/pin/create"
+// //           className={({ isActive }) =>
+// //             isActive ? "nav-bar__link--active" : "nav-bar__link--inactive"
+// //           }
+// //         >
+// //           Create
+// //         </NavLink>
+
+// //         <NavLink
+// //           to="/todo"
+// //           className={({ isActive }) =>
+// //             isActive ? "nav-bar__link--active" : "nav-bar__link--inactive"
+// //           }
+// //         >
+// //           ToDo
+// //         </NavLink>
+
+// //       </div>
+// //       <div className="nav-bar__search-box">
+// //         <SearchIcon />
+// //         <form onSubmit={handleSearch}>
+// //           <input
+// //             type="text"
+// //             placeholder="Search"
+// //             name="query"
+// //             defaultValue={query || ""}
+// //           />
+// //           <button type="submit"></button>
+// //         </form>
+// //       </div>
+// //       <div className="nav-bar__icon-group">
+// //         <div className="nav-bar__icon nav-bar__icon--gray">
+// //           <Link to="/profile">
+// //             <AccountCircleIcon />
+// //           </Link>
+// //         </div>
+// //         <div
+// //           className="nav-bar__icon nav-bar__icon--gray"
+// //           onClick={handleLogout}
+// //         >
+// //           <LogoutIcon />
+// //         </div>
+// //       </div>
+// //     </div>
+// //   );
+// // };
+
+// // export default NavBar;
+// import React, { useEffect, useState } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+// import LogoutIcon from "@mui/icons-material/Logout";
+// import PinterestIcon from "@mui/icons-material/Pinterest";
+// import SearchIcon from "@mui/icons-material/Search";
+// import { Link, NavLink, useNavigate } from "react-router-dom";
+// import "./NavBar.css";
+// import { logout } from "../actions/session";
+
+// const NavBar = ({ query }) => {
+//   const dispatch = useDispatch();
+//   const navigate = useNavigate();
+//   const user = useSelector((state) => state.session.user);
+
+//   const [showMenu, setShowMenu] = useState(false);
+
+//   const handleSearch = (event) => {
+//     event.preventDefault();
+//     navigate(`/search/${event.target.query.value}`);
+//   };
+
+//   const handleLogout = () => dispatch(logout());
+
+//   const toggleMenu = () => setShowMenu(!showMenu);
+
+//   return (
+//     <div className="nav-bar">
+//       <div className="nav-bar__icon nav-bar__icon--red">
+//         <Link to="/">
+//           <PinterestIcon />
+//         </Link>
+//       </div>
+//       <div className="nav-bar__link">
+//         <NavLink
+//           to="/"
+//           className={({ isActive }) =>
+//             isActive ? "nav-bar__link--active" : "nav-bar__link--inactive"
+//           }
+//         >
+//           Home
+//         </NavLink>
+//         <NavLink
+//           to="/pin/create"
+//           className={({ isActive }) =>
+//             isActive ? "nav-bar__link--active" : "nav-bar__link--inactive"
+//           }
+//         >
+//           Create
+//         </NavLink>
+//         <NavLink
+//           to="/todo"
+//           className={({ isActive }) =>
+//             isActive ? "nav-bar__link--active" : "nav-bar__link--inactive"
+//           }
+//         >
+//           ToDo
+//         </NavLink>
+//       </div>
+//       <div className="nav-bar__search-box">
+//         <SearchIcon />
+//         <form onSubmit={handleSearch}>
+//           <input
+//             type="text"
+//             placeholder="Search"
+//             name="query"
+//             defaultValue={query || ""}
+//           />
+//           <button type="submit"></button>
+//         </form>
+//       </div>
+//       <div className="nav-bar__icon-group">
+//         <div
+//           className="nav-bar__icon nav-bar__icon--gray"
+//           onClick={toggleMenu}
+//           onMouseEnter={() => setShowMenu(true)}
+//           onMouseLeave={() => setShowMenu(false)}
+//         >
+//           <AccountCircleIcon />
+//           {showMenu && (
+//             <div className="dropdown-menu">
+//               <p>{user.name}</p>
+//               <p>{user.username}</p>
+//               <div className="dropdown-section">
+//                 <button onClick={() => navigate('/profile')}>Profile</button>
+//                 <button onClick={() => navigate('/')}>All Pins</button>
+//               </div>
+//               <div className="dropdown-section">
+//                 <button onClick={handleLogout}>Log out</button>
+//               </div>
+//             </div>
+//           )}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default NavBar;
+
 import React,{ useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
@@ -114,3 +303,6 @@ const NavBar = ({ query }) => {
 };
 
 export default NavBar;
+
+
+
