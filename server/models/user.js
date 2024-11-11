87499ccd538a9adc5375339e2e5666c87dd2bc18
pso011 <poojithasoma2805@gmail.com>
2024-11-10 22:19:08 -0600

    Update comments user.js

--- server/models/user.js
+++ server/models/user.js
@@ -1,8 +1,17 @@
 const mongoose = require("mongoose");
 const uniqueValidator = require("mongoose-unique-validator");
 
+/ Define a schema for comments
+const commentSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" }, // Reference to user who made the comment
+  username: String,
+  avatarUrl: String,
+  text: { type: String, required: true },
+  timestamp: { type: Date, default: Date.now },
+  likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }]
+});
 
-
+// Define the schema for pins, with comments as an array of `commentSchema`
 const pinSchema = new mongoose.Schema({
   title: {
     type: String,
@@ -16,8 +25,10 @@ const pinSchema = new mongoose.Schema({
     type: Date,
     default: Date.now,
   },
+  comments: [commentSchema] // Array of comments for each pin
 });
 
+// Define the schema for users
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
@@ -41,12 +52,15 @@ const userSchema = new mongoose.Schema({
   customPins: {
     type: [pinSchema], // Array of custom pins based on pinSchema
   },
+  hiddenPins: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Pin' }],
   followers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }], // List of users following this user
   following: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }], // List of users this user is following
 });
 
+/ Plugin for enforcing unique constraints on fields
 userSchema.plugin(uniqueValidator);
 
+// Transform JSON output to hide sensitive data
 userSchema.set("toJSON", {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString();
