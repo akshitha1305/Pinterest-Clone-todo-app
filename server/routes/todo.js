41547a0a6101ade897983ae826f6f5909461080e
poojitha soma <poojithasoma2805@gmail.com>
2024-11-13 16:36:38 -0600

    Toggle todo status todo.js

--- server/routes/todo.js
+++ server/routes/todo.js
@@ -9,6 +9,19 @@ router.get('/', (req, res) => {
 });
 
 
+
+
+// Toggle todo status
+router.put('/:id', (req, res) => {
+  const todo = todos.find(todo => todo.id == req.params.id);
+  if (todo) {
+    todo.isChecked = !todo.isChecked;
+    res.json(todo);
+  } else {
+    res.status(404).send("Todo not found");
+  }
+});
+
 // Delete a todo
 router.delete('/:id', (req, res) => {
     todos = todos.filter(todo => todo.id != req.params.id);
